47ed01911794194b8da670a1e6ddb7bce8c50999
Bartek <bartosz.nowacki2@gmail.com>
2019-04-12 10:44:03 +0200

    Initial Commit

---
+++ RecruitNetguru/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecruitNetguru
+//
+//  Created by Bartosz Nowacki on 12/04/2019.
+//  Copyright © 2019 Bartosz Nowacki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
